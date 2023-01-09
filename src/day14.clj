c5dd4811625e2b3e81de4334df9414c6c82ad8c3
Steve Marin <steve.marin@gmail.com>
2023-01-08 23:22:18 -0600

    cleanup

--- src/day14.clj
+++ src/day14.clj
@@ -27,46 +27,41 @@
        (partition 4 2)))
 
 (defn set-slice!
-  [cave offsets rows cols]
+  [cave offset-col rows cols]
   (let [[start-row end-row] rows
         [start-col end-col] cols
-        [offset-row offset-col] offsets
         min-row (min start-row end-row)
         max-row (max start-row end-row)
         min-col (min start-col end-col)
         max-col (max start-col end-col)]
     (doseq [row (range min-row (inc max-row))
             col (range min-col (inc max-col))]
-      (m/mset! cave (- row offset-row) (- col offset-col) 1.0)))
+      (m/mset! cave row (- col offset-col) 1.0)))
   cave)
 
 (defn one-formation
-  [cave offsets formation]
+  [cave offset-col formation]
   (doseq
    [part formation]
     (let [[start-col start-row end-col end-row] part]
-      (set-slice! cave offsets [start-row end-row] [start-col end-col])))
+      (set-slice! cave offset-col [start-row end-row] [start-col end-col])))
   cave)
 
 (defn all-formations
   [formations]
   (let [[start-row end-row start-col end-col] (all-extremes formations)
-        min-row 0
-        max-row (+ 3 (max start-row end-row))
+        num-rows (+ 3 (max start-row end-row))
         min-col (min start-col end-col)
         max-col (max start-col end-col)
 
-        offset-rows 0
         offset-cols 150
-        offset-row (- min-row offset-rows)
-        num-rows (- max-row offset-row)
         offset-col (- min-col offset-cols)
         num-cols (- max-col offset-col)
 
-        shape [(+ num-rows offset-rows) (+ num-cols offset-cols)]
+        shape [num-rows (+ num-cols offset-cols)]
         cave (m/mutable (m/zero-array shape))]
-    (doseq [formation formations] (one-formation cave [offset-row offset-col] formation))
-    {:cave cave :offset-row offset-row :offset-col offset-col}))
+    (doseq [formation formations] (one-formation cave offset-col formation))
+    {:cave cave :offset-col offset-col}))
 
 (defn add-floor
   [cave]
@@ -111,13 +106,12 @@
            num-drops 0]
       (cond
         (nil? loc) (do
-                     (println "on to drop" (inc num-drops))
                      (m/mset! cave (first prev-loc) (second prev-loc) 1.0)
                      (recur cave start-loc (new-location start-loc cave) (inc num-drops)))
         (= max-row (first loc)) num-drops
         :else (recur cave loc (new-location prev-loc cave) num-drops)))))
 
-(part-one "day14_sample.txt")
+(assert (= 24 (part-one "day14_sample.txt")))
 (part-one "day14.txt")
 
 (defn part-two
@@ -130,36 +124,24 @@
            loc (new-location start-loc cave)
            num-drops 0]
       (cond
-        (= (m/mget cave 0 (second start-loc)) 1.0) cave
+        (= (m/mget cave 0 (second start-loc)) 1.0) num-drops
         (nil? loc) (do
-                     (println "on to drop" (inc num-drops))
                      (m/mset! cave (first prev-loc) (second prev-loc) 1.0)
                      (recur cave start-loc (new-location start-loc cave) (inc num-drops)))
         :else (recur cave loc (new-location prev-loc cave) num-drops)))))
 
-(def c (part-two "day14_sample.txt"))
-(def c (part-two "day14.txt"))
+(assert (= 93 (part-two "day14_sample.txt")))
+(part-two "day14.txt")
 
-(def cave (get-cave "day14_sample.txt"))
-(def cave (get-cave "day14.txt"))
-cave
+;; fun cave drawing stuff
+;; (def cave (get-cave "day14_sample.txt"))
+;; (def cave (get-cave "day14.txt"))
+;; cave
 
-(doseq [tmp (->> (:cave cave)
-                 flatten
-                 (replace {0.0 "." 1.0 "#"})
-                 (partition (m/column-count (:cave cave)))
-                 (map str/join)
-                 )]
-  (println tmp))
-
-c
-(doseq [tmp (->> c
-                 flatten
-                 (replace {0.0 "." 1.0 "#"})
-                 (partition (m/column-count c))
-                 (map str/join)
-                 )]
-  (println tmp))
-
-(map #(reduce + %) (m/rows (:cave cave)))
-
+;; (doseq [tmp (->> (:cave cave)
+;;                  flatten
+;;                  (replace {0.0 "." 1.0 "#"})
+;;                  (partition (m/column-count (:cave cave)))
+;;                  (map str/join)
+;;                  )]
+;;   (println tmp))
